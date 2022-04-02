38577f09d633ad131bbd27d7575f7ef1e3fce165
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-02 13:29:08 +0530

    know about feilds and add blinking features using js and add link also

--- tut20/gs20/enroll/views.py
+++ tut20/gs20/enroll/views.py
@@ -5,3 +5,6 @@ def home(request):
     form = StudentRegistration(initial={'name': 'Enter your full name'})
     # yaha initial value ki property jyadha rahti h.
     return render(request, 'enroll/home.html',{'form':form})
+
+    # https://stackoverflow.com/questions/8360130/how-to-make-a-text-flash-in-html-javascript
+    
