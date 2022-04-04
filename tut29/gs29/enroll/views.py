ba846c8cfe400adf9bf78dd0d675f642c2d99843
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-04 09:44:11 +0530

    Add custom validation in forms.py class

---
+++ tut29/gs29/enroll/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import StudentRegistration
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = StudentRegistration(request.POST)
+        if form.is_valid():
+            pass 
+    else:
+        form = StudentRegistration()
+    return render(request, 'enroll/index.html',{'form':form})
