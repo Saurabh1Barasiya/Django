0ef8553419929b4ba019ce766ebc99b6fa8cb17b
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-04 13:54:14 +0530

    Show errors using for loop

--- tut31/gs31/enroll/forms.py
+++ tut31/gs31/enroll/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 class StudentRegistration(forms.Form):
-    name = forms.CharField(error_messages={'required': 'Please enter your name'})
+    name = forms.CharField(min_length=5,error_messages={'required': 'Please enter your name','min_length': 'Name should be atleast 5 characters'})
     email = forms.EmailField(error_messages={'required': 'Please enter your Email address'})
     password = forms.CharField(error_messages={'required': 'Please enter your password'})
