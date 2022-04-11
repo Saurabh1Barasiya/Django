b476860ca916a41b9be7eb2bef14aaa019863b1f
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-04-11 15:02:00 +0530

    Adding profile functionality using UserProfileForm in forms.py file

---
+++ tut48/gs43/enroll/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+class StudentRegistration(UserCreationForm):
+    password1  = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2  = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ['username', 'first_name','last_name','email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),  # 'class': 'form-control'
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+
+        }
+
+class EditUserProfileForm(UserChangeForm):
+    password = None
+    class Meta:
+        model = User
+        fields = ['username', 'first_name','last_name','email','date_joined','last_login','is_superuser','is_staff','is_active']
+        labels = {
+            'email': 'Email',
+            }
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),  # 'class': 'form-control'
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'date_joined': forms.DateInput(attrs={'class': 'form-control'}),
+            'last_login': forms.DateInput(attrs={'class': 'form-control'}),
+            'is_superuser': forms.CheckboxInput(attrs={'class': 'form-check-input '}),
+            'is_staff': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
---
+++ tut48/gs43/gs43/urls.py
@@ -0,0 +1,27 @@
+"""gs43 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from enroll import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('user_login/', views.user_login, name='login'),
+    path('profile_page/', views.profile_page, name='profile'),
+    path('User_logout/', views.User_logout, name='User_logout'),
+    path('change_pass_with_old/', views.change_passs_with_the_help_of_old_pass, name='change_pass_with_old'),
+    path('change_pass_without_old/', views.change_pass_without_old, name='change_pass_without_old'),
+]
